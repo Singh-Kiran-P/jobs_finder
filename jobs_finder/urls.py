6912f2c60c7089b10805225ef9709b5a0825b084
Singh Kiran <singh.kiran2456@hotmail.com>
2020-07-01 22:01:12 +0200

    Renamed project to jobs_finder

--- jobs_finder/urls.py
+++ jobs_finder/urls.py
@@ -1,4 +1,4 @@
-"""craiglist_clone URL Configuration
+"""jobs_finder URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
